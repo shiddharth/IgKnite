fd1caaddec463eadb8707ca250d5c4130f105c70
Anindya Shiddhartha <>
2021-05-21 09:50:24 +0000

    Reduced sensitivity of swear filter.

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ async def on_message(message):
                         msg_word = msg_word.replace(symbol, '')
 
                 for filtered_word in filtered_wordlist:
-                    if filtered_word.lower() in msg_word.lower():
+                    if filtered_word.lower() == msg_word.lower():
                         filtered_messages.append([message.author, message.guild, message.content])
                         await message.delete()
                         skip_command = True
